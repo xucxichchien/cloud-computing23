9ecc3589c50f27dcb8c660934bcd52edb7c6497a
xucxichchien <111264991+xucxichchien@users.noreply.github.com>
2022-09-21 13:44:25 +0700

    Add files via upload

--- appserver.js
+++ appserver.js
@@ -4,22 +4,6 @@ const appServer = express();
 const router = express.Router();
 const fs = require("fs");
 
-// Middleware
-const bodyParser = require("body-parser");
-
-
-// ------------------- Middleware - kiem soat tinh huong
-router.use( (yeucau, trave, ketiep) => { 
-    console.log("REQ: ", Date.now(), yeucau.url);
-    ketiep();
-});
-
-router.use( (loixayra, yeucau, trave, ketiep) => { 
-    console.log("ERROR: ", Date.now(), yeucau.url);
-    console.log(loixayra);
-    trave.status(500).send("Dang co loi xay ra, chua biet o dau !!!");
-});
-
 // ------------------- Routing
 router.get( "/" , (yeucau, trave) => {
     data = fs.readFileSync("html/main.html");
@@ -33,22 +17,43 @@ router.get( "/home" , (yeucau, trave) => {
     trave.send(pageContent);
 });
 
+router.get( "/products" , (yeucau, trave) => {
+    data = fs.readFileSync("html/products.html");
+    pageContent = data.toString();
+    trave.send(pageContent);
+});
 
-//--- Add middleware
-//const session = express.session();
-appServer.use(bodyParser.json());
-//appServer.use(session({secret: "id-session-Mr.Tu"​}));
+router.get( "/login" , (yeucau, trave) => {
+    data = fs.readFileSync("html/login.html");
+    pageContent = data.toString();
+    trave.send(pageContent);
+});
 
 
-// ------------------------- Add Router / Controller
+
+
+// -------------------------
+
+// ----------------Middleware
+//const express = require('express');
+//const app = express();
+//const router = express.Router();
+
+//router.use((yeucau, trave, next) => {
+    //console.log('Time: ', Date.now());
+   // next();
+//});
+
+//router.get('home', (yeucau,trave) => {
+   // res.send("ok");
+//});
+
+//app.use('/', router);
+//----------------------
 appServer.use("/", router);
 
-const ProductRouter = require("./controller/productController").ProductRouter;
+const ProductRouter = require("./controller/productController");
 appServer.use("/products", ProductRouter);
-
-const LoginRouter = require("./controller/loginController").LoginRouter;
-appServer.use("/login", LoginRouter);
-
 // ----------- RUN / Launching !!! 
 appServer.listen( PORT );
 
