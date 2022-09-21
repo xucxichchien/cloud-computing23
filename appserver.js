2a18b6218dd3787ec443eea245cf6e737c75bdec
xucxichchien <111264991+xucxichchien@users.noreply.github.com>
2022-09-21 13:30:32 +0700

    Add files via upload

--- appserver.js
+++ appserver.js
@@ -4,50 +4,45 @@ const appServer = express();
 const router = express.Router();
 const fs = require("fs");
 
+// ------------------- Middleware - kiem soat tinh huong
+router.use( (yeucau, trave, ketiep) => { 
+    console.log("REQ: ", Date.now(), yeucau.url);
+    ketiep();
+});
+
+router.use( (loixayra, yeucau, trave, ketiep) => { 
+    console.log("ERROR: ", Date.now(), yeucau.url);
+    console.log(loixayra);
+    trave.status(500).send("Dang co loi xay ra, chua biet o dau !!!");
+});
+
 // ------------------- Routing
 router.get( "/" , (yeucau, trave) => {
-    data = fs.readFileSync("html/main.html");
+    data = fs.readFileSync("./html/main.html");
     pageContent = data.toString();
     trave.send(pageContent);
 });
 
 router.get( "/home" , (yeucau, trave) => {
-    data = fs.readFileSync("html/home.html");
-    pageContent = data.toString();
-    trave.send(pageContent);
-});
-
-router.get( "/products" , (yeucau, trave) => {
-    data = fs.readFileSync("html/products.html");
+    data = fs.readFileSync("./html/home.html");
     pageContent = data.toString();
     trave.send(pageContent);
 });
 
 router.get( "/login" , (yeucau, trave) => {
-    data = fs.readFileSync("html/login.html");
+    data = fs.readFileSync("./html/login.html");
+    pageContent = data.toString();
+    trave.send(pageContent);
+});
+
+router.get( "/products" , (yeucau, trave) => {
+    data = fs.readFileSync("./html/products.html");
     pageContent = data.toString();
     trave.send(pageContent);
 });
 
 
 // -------------------------
-
-// ----------------Middleware
-//const express = require('express');
-//const app = express();
-//const router = express.Router();
-
-//router.use((yeucau, trave, next) => {
-    //console.log('Time: ', Date.now());
-   // next();
-//});
-
-//router.get('home', (yeucau,trave) => {
-   // res.send("ok");
-//});
-
-//app.use('/', router);
-//----------------------
 appServer.use("/", router);
 // ----------- RUN / Launching !!! 
 appServer.listen( PORT );
