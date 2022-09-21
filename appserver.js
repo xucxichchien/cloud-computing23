cc87675df2f07e6552611f18b42aa398e2a2a177
xucxichchien <111264991+xucxichchien@users.noreply.github.com>
2022-09-21 13:09:01 +0700

    Add files via upload

--- appserver.js
+++ appserver.js
@@ -6,19 +6,19 @@ const fs = require("fs");
 
 // ------------------- Routing
 router.get( "/" , (yeucau, trave) => {
-    data = fs.readFileSync("main.html");
+    data = fs.readFileSync("html/main.html");
     pageContent = data.toString();
     trave.send(pageContent);
 });
 
 router.get( "/home" , (yeucau, trave) => {
-    data = fs.readFileSync("home.html");
+    data = fs.readFileSync("html/home.html");
     pageContent = data.toString();
     trave.send(pageContent);
 });
 
 router.get( "/products" , (yeucau, trave) => {
-    data = fs.readFileSync("products.html");
+    data = fs.readFileSync("html/products.html");
     pageContent = data.toString();
     trave.send(pageContent);
 });
