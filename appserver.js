b526c99eafb910782f8fa0e6b350ae74c48e6153
xucxichchien <111264991+xucxichchien@users.noreply.github.com>
2022-09-21 13:39:50 +0700

    Add files via upload

--- appserver.js
+++ appserver.js
@@ -4,6 +4,10 @@ const appServer = express();
 const router = express.Router();
 const fs = require("fs");
 
+// Middleware
+const bodyParser = require("body-parser");
+
+
 // ------------------- Middleware - kiem soat tinh huong
 router.use( (yeucau, trave, ketiep) => { 
     console.log("REQ: ", Date.now(), yeucau.url);
@@ -35,15 +39,23 @@ router.get( "/login" , (yeucau, trave) => {
     trave.send(pageContent);
 });
 
-router.get( "/products" , (yeucau, trave) => {
-    data = fs.readFileSync("./html/products.html");
-    pageContent = data.toString();
-    trave.send(pageContent);
-});
 
 
-// -------------------------
+
+//--- Add middleware
+//const session = express.session();
+appServer.use(bodyParser.json());
+//appServer.use(session({secret: "id-session-Mr.Tu"​}));
+
+
+// ------------------------- Add Router / Controller
 appServer.use("/", router);
+
+const ProductRouter = require("./controller/productController").ProductRouter;
+
+appServer.use("/products", ProductRouter);
+
+
 // ----------- RUN / Launching !!! 
 appServer.listen( PORT );
 
